e43b825179bdb9bf8d8aff32c51912d01f039345
JusicP <slender87844@gmail.com>
2025-09-10 15:24:23 +0300

    Server: create sqlalchemy `session` fixture that creates sqlite db in memory

--- server/tests/conftest.py
+++ server/tests/conftest.py
@@ -1,19 +1,42 @@
 
 import pytest
+import pytest_asyncio
+
+from typing import AsyncGenerator
+from fastapi import FastAPI
 from fastapi.testclient import TestClient
-from main import app
-from server.database.db import Base, async_engine
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 
+from server.main import app
+from server.database.db import Base, generate_async_session
+
+
+@pytest_asyncio.fixture(scope="function")
+async def session():
+    engine = create_async_engine("sqlite+aiosqlite://")
+    async with engine.connect() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+        async_session = AsyncSession(
+            conn,
+            expire_on_commit=False
+        )
+
+        yield async_session
+
+        await async_session.close()
+        await conn.run_sync(Base.metadata.drop_all)
+
+@pytest.fixture()
+def test_app(session: AsyncSession) -> FastAPI:
+    async def test_generate_async_session() -> AsyncGenerator[AsyncSession, None]:
+        yield session
+              
+    app.dependency_overrides[generate_async_session] = test_generate_async_session
+
+    return app
 
 @pytest.fixture(scope="function")
-def session():
-    ...
-
-@pytest.fixture(scope="function")
-def client():
-    Base.metadata.create_all(bind=async_engine)
-
-    with TestClient(app) as test_client:
+def client(test_app: FastAPI):
+    with TestClient(test_app) as test_client:
         yield test_client
-
-    Base.metadata.drop_all(bind=async_engine)
