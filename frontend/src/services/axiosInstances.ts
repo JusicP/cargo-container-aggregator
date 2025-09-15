85c87eef9c0cd1a9690a12a3b29348c35c966380
OnishchenkoDaria <daria.onishchenko.57@gmail.com>
2025-09-15 18:10:22 +0300

    feat: more specific interceptor logic, more proper error handling, set up instance moved

--- frontend/src/services/axiosInstances.ts
+++ frontend/src/services/axiosInstances.ts
@@ -1,19 +1,25 @@
 import axios from "axios";
+import {requestInterceptor, responseInterceptor, errorInterceptor} from "./axiosInterceptors.ts";
 
+//for non-registered users operations
 export const defaultAxiosInstance = axios.create({
     baseURL: import.meta.env.VITE_SERVER_URL,
-    timeout: 5000,
+    //timeout: 5000,
     withCredentials: true,
     headers: {
         "Content-Type": "application/json"
     }
 });
 
+//for operations which require token
 export const privateAxiosInstance = axios.create({
     baseURL: import.meta.env.VITE_SERVER_URL,
-    timeout: 5000,
+    //timeout: 5000,
     withCredentials: true,
     headers: {
         "Content-Type": "application/json"
     }
 });
+
+privateAxiosInstance.interceptors.request.use(requestInterceptor, errorInterceptor);
+privateAxiosInstance.interceptors.response.use(responseInterceptor, errorInterceptor);
--- frontend/src/services/axiosInterceptors.ts
+++ frontend/src/services/axiosInterceptors.ts
@@ -1,25 +1,88 @@
-import {privateAxiosInstance} from "./axiosInstances.ts";
-import type {AxiosError, InternalAxiosRequestConfig, AxiosResponse} from "axios";
+//import {defaultAxiosInstance} from "./axiosInstances.ts";
+import type {AxiosError, InternalAxiosRequestConfig, AxiosResponse, AxiosRequestConfig} from "axios";
+import axios from "axios";
 
-privateAxiosInstance.interceptors.request.use(
+// For Make Log on Develop Mode
+const logOnDev = (message: string) => {
+    if (import.meta.env.MODE === "development") {
+        console.log(message);
+    }
+};
+
+export const requestInterceptor =
     async (config: InternalAxiosRequestConfig) => {
-        //temporary, waiting for backend implementation of refresh token
+        const { method, url } = config;
+
         const accessToken = sessionStorage.getItem("accessToken")
         if (accessToken) {
+            //setting token inside headers
             config.headers['Authorization'] = `Bearer ${accessToken}`;
+        } else {
+            //waiting for backend implementation -- call to refresh token
+            /*
+                const newAccessToken = defaultAxiosInstance.put()
+                sessionStorage.setItem("accessToken", newAccessToken);
+            */
         }
-        return config;
-    }, async (error: AxiosError) => {
-        return Promise.reject(error);
-    }
-)
+        //logging for debugging purposes (development mode)
+        logOnDev(`[axios API] ${method?.toUpperCase()} ${url} | Request`);
+        if (method === "get") {
+            config.timeout = 15000;
+        }
 
-privateAxiosInstance.interceptors.response.use(
-    async (config: AxiosResponse) => {
         return config;
-    }, async (error: AxiosError) => {
-        if (error.response?.status === 401) {
-            //call refresh
-        }
     }
-)
+
+export const responseInterceptor =
+    async (response: AxiosResponse) => {
+        const { method, url } = response.config;
+        const { status } = response;
+
+        //logging for debugging purposes (development mode)
+        logOnDev(`[axios API] ${method?.toUpperCase()} ${url} | Response ${status}`);
+
+        //TO DO: additional response processing and formatting (in question)
+
+        return response;
+    }
+
+export const errorInterceptor =
+async (error: AxiosError | Error) => {
+    if(axios.isAxiosError(error)) {
+        const { message } = error;
+        const { method, url } = error.config as AxiosRequestConfig;
+        const { status } = error.response as AxiosResponse ?? {};
+
+        //logging for debugging purposes (development mode)
+        logOnDev(
+            ` [axios API] ${method?.toUpperCase()} ${url} | Error ${status} ${message}`
+        );
+        switch (status) {
+            case 401: {
+                //unauthenticated user, expired token
+                sessionStorage.removeItem("accessToken");
+                //navigate user (when router will be done)
+                break;
+            }
+            case 403: {
+                //access denied
+                break;
+            }
+            case 404: {
+                //invalid request
+                break;
+            }
+            case 500: {
+                //server error
+                break;
+            }
+            default: {
+                //unknown error
+                break;
+            }
+        }
+    } else {
+        logOnDev(`[axios API] | Error ${error.message}`);
+    }
+    return Promise.reject(error);
+}
