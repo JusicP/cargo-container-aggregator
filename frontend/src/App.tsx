dcb0af9ac78297c32f629c157b5a603f688c0084
Artem Verbytskyi <116598786+Ke11nyk@users.noreply.github.com>
2025-09-15 23:21:33 +0300

    feat: create router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
-import './App.css'
+import React from 'react';
+import AppRouter from './router/AppRouter';
+import './App.css';
 
-function App() {
+const App: React.FC = () => {
   return (
-    <>
-      Cargo React App
-    </>
-  )
-}
+    <div className="App">
+      <AppRouter />
+    </div>
+  );
+};
 
-export default App
+export default App;
