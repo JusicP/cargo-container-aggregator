d6a8784aa62ddefda48c601fc3743cd4b5fcd834
JusicP <slender87844@gmail.com>
2025-09-29 16:14:14 +0300

    Server: create `UserRegister` and `UserCreate` schema on top of it to allow specifying `role` field

--- server/schemas/user.py
+++ server/schemas/user.py
@@ -19,9 +19,12 @@ class UserBase(BaseModel):
     company_name: str | None = Field(max_length=64)
     avatar_photo_id: int | None
 
-class UserCreate(UserBase):
+class UserRegister(UserBase):
     password: str = Field(max_length=128)
     avatar_photo_id: int | None = Field(None, exclude=True) # don't allow to set avatar_photo_id on creation, we allow it after creation only
+
+class UserCreate(UserRegister):
+    role: str = "user"
 
 class UserGet(UserBase):
     id: int
--- server/utils/default_admin.py
+++ server/utils/default_admin.py
@@ -3,7 +3,6 @@ from server.schemas.user import UserCreate
 from server.services.user_service import create_user, get_user_by_email
 
 async def ensure_superuser(session):
-  
     email = os.getenv("SUPERUSER_EMAIL", "admin@example.com")
     password = os.getenv("SUPERUSER_PASSWORD", "12345678")
     name = os.getenv("SUPERUSER_NAME", "Admin")
@@ -19,11 +18,9 @@ async def ensure_superuser(session):
         password=password,
         phone_number="0000000000",
         company_name=None,
-        avatar_photo_id=None
+        avatar_photo_id=None,
+        role="user"
     )
 
-    user_data = user_create.model_dump()
-    user_data["role"] = "admin"
-
-    await create_user(session, UserCreate(**user_data))
+    await create_user(session, user_create)
     print(f"Superuser {email} created")
