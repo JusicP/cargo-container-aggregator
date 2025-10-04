ebf1c0a9de6a79f5d3d26244a0515a2097ae99e4
Illia Andrieiev <i.e.andreev2005@gmail.com>
2025-10-04 20:47:31 +0300

    pull main

--- server/models/refresh_token.py
+++ server/models/refresh_token.py
@@ -1,8 +1,14 @@
 
 import datetime
+<<<<<<< HEAD
 from sqlalchemy import String, Boolean, DateTime, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column
 from server.database.connection import Base
+=======
+from sqlalchemy import String, Integer, Boolean, DateTime, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column
+from server.database.db import Base
+>>>>>>> 18dee23 (conflicts)
 
 class RefreshToken(Base):
     __tablename__ = "refresh_tokens"
@@ -10,6 +16,7 @@ class RefreshToken(Base):
     id: Mapped[int | None] = mapped_column(primary_key=True)
     user_id: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=False)
     token: Mapped[str] = mapped_column(String(256), unique=True, nullable=False, index=True)
+<<<<<<< HEAD
     issued_at: Mapped[datetime.datetime] = mapped_column(DateTime, default=datetime.datetime.now(datetime.timezone.utc))
     expires_at: Mapped[datetime.datetime] = mapped_column(DateTime, nullable=False)
     revoked: Mapped[bool] = mapped_column(Boolean, default=False)
@@ -17,3 +24,8 @@ class RefreshToken(Base):
     @property
     def expires_at_aware(self):
         return self.expires_at.replace(tzinfo=datetime.timezone.utc)
+=======
+    issued_at: Mapped[datetime.datetime] = mapped_column(DateTime, default=datetime.datetime.utcnow)
+    expires_at: Mapped[datetime.datetime] = mapped_column(DateTime, nullable=False)
+    revoked: Mapped[bool] = mapped_column(Boolean, default=False)
+>>>>>>> 18dee23 (conflicts)
