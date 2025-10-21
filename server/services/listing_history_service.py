fbb23b1aa0050dc70157e502d6bd95b69a8f3238
VikaBilyk <128689260+VikaBilyk@users.noreply.github.com>
2025-10-22 00:00:11 +0300

    update listing history

--- server/services/listing_history_service.py
+++ server/services/listing_history_service.py
@@ -1,18 +1,34 @@
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from server.models.listing import Listing
 from server.models.listing_history import ListingHistory
 
-async def create_listing_history(session: AsyncSession, listing: Listing):
+async def create_listing_history(session: AsyncSession, listing: Listing, use_last_analytics: bool = False):
+    if use_last_analytics:
+
+        last_history = await session.execute(
+            select(ListingHistory)
+            .where(ListingHistory.listing_id == listing.id)
+            .order_by(ListingHistory.addition_date.desc())
+        )
+        last_history = last_history.scalars().first()
+        views = last_history.views if last_history else 0
+        contacts = last_history.contacts if last_history else 0
+        favorites = last_history.favorites if last_history else 0
+    else:
+        views = 0
+        contacts = 0
+        favorites = 0
+
     history = ListingHistory(
         listing_id=listing.id,
         price=listing.price,
-        views=0,
-        contacts=0,
-        favorites=0,
+        views=views,
+        contacts=contacts,
+        favorites=favorites,
         addition_date=listing.addition_date,
     )
-
     session.add(history)
     await session.commit()
     await session.refresh(history)
