287d6e0fcbd6236b379eae723dfa72607881186c
OnishchenkoDaria <daria.onishchenko.57@gmail.com>
2025-10-22 09:50:11 +0300

    feat: replaces default inputs in register form with chakra ui ones, added styling to the App

--- frontend/src/components/authentication/RegisterForm.tsx
+++ frontend/src/components/authentication/RegisterForm.tsx
@@ -2,6 +2,22 @@ import {useForm} from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import {registerUserSchema, type registerUser} from "../../schemas/authUserSchema.ts";
 import DOMPurify from "dompurify";
+import { Field, Input, InputGroup } from "@chakra-ui/react"
+import { Stack } from "@chakra-ui/react"
+import { type Options, passwordStrength } from "check-password-strength"
+import {
+    PasswordInput,
+    PasswordStrengthMeter,
+} from "@/components/ui/password-input"
+import { useMemo, useState } from "react"
+import { Button } from "@chakra-ui/react"
+
+const strengthOptions: Options<string> = [
+    { id: 1, value: "weak", minDiversity: 0, minLength: 0 },
+    { id: 2, value: "medium", minDiversity: 2, minLength: 6 },
+    { id: 3, value: "strong", minDiversity: 3, minLength: 8 },
+    { id: 4, value: "very-strong", minDiversity: 4, minLength: 10 },
+]
 
 export default function RegisterForm() {
     const { handleSubmit,register, formState: { errors } } = useForm({
@@ -19,24 +35,55 @@ export default function RegisterForm() {
             
         }
     }
+    const [password, setPassword] = useState("")
+
+    const strength = useMemo(() => {
+        if (!password) return 0
+        const result = passwordStrength(password, strengthOptions)
+        return result.id
+    }, [password])
 
     return(
-        <form onSubmit={handleSubmit(onSubmit)}>
+        <form onSubmit={handleSubmit(onSubmit)} className="w-80 px-20">
             <div>
-                <label htmlFor="email">Email</label>
-                <input {...register("email")} />
+                <Field.Root required>
+                    <Field.Label>
+                        Email <Field.RequiredIndicator />
+                    </Field.Label>
+                    <InputGroup endAddon=".com">
+                        <Input
+                            {...register("email")}
+                            placeholder="email@email"
+                            size="sm"
+                        />
+                    </InputGroup>
+                </Field.Root>
                 {errors.email && <p role="alert">{errors.email.message}</p>}
             </div>
 
             <div>
-                <label htmlFor="password">Password</label>
-                <input {...register("password")} />
+                <Field.Root required>
+                    <Field.Label>
+                        Password <Field.RequiredIndicator />
+                    </Field.Label>
+                    <Stack gap="3" className="w-full">
+                        <PasswordInput
+                            value={password}
+                            onChange={(e) => setPassword(e.currentTarget.value)}
+                            placeholder="Enter your password"
+                            size="sm"
+                        />
+                        <PasswordStrengthMeter value={strength} />
+                    </Stack>
+                </Field.Root>
                 {errors.password && <p role="alert">{errors.password.message}</p>}
             </div>
 
-            <div>
-                <input type="submit" value="Register"/>
-            </div>
+            <Button
+                type="submit"
+            >
+                Реєстація
+            </Button>
         </form>
     )
 }
--- frontend/src/pages/RegisterPage.tsx
+++ frontend/src/pages/RegisterPage.tsx
@@ -1,3 +1,12 @@
+import RegisterForm from "@/components/authentication/RegisterForm.tsx";
+import PageLayout from "@/router/PageLayout.tsx";
+
 export default function RegisterPage() {
-    return <div></div>;
+    return (
+        <PageLayout>
+            <div className="bg-white h-screen px-20">
+                <RegisterForm />
+            </div>
+        </PageLayout>
+    );
 }
