2548cfd6da007e6e5371e19ba11d5f01b2866c88
Artem Verbytskyi <116598786+Ke11nyk@users.noreply.github.com>
2025-10-25 11:48:08 +0300

    Frontend: add multi language i18next

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,8 @@ import './index.css'
 import App from './App.tsx'
 // connecting Chakra UI library - icons & components
 import { Provider } from "@/components/ui/provider"
+// configuration i18next
+import './i18n/config'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
