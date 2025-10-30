bde0bbbbceb3a2d3d63977aba39521afadbb3e6a
OnishchenkoDaria <daria.onishchenko.57@gmail.com>
2025-10-30 16:24:16 +0200

    feat: adding template photo of container & fixing footer styling

--- frontend/src/pages/homepage/HomePage.tsx
+++ frontend/src/pages/homepage/HomePage.tsx
@@ -1,7 +1,8 @@
 import {useState} from "react";
 import background from "@/assets/background.png";
 import '@/pages/homepage/homepage.css'
-
+import container from "@/assets/container.png";
+import { BrandGithub, BrandGoogle, BrandFacebook, BrandTwitter } from "@mynaui/icons-react";
 
 function App() {
     const [lang, setLang] = useState("UA");
@@ -158,7 +159,7 @@ function App() {
                             {[...Array(6)].map((_, i) => (
                                 <div key={i} className="product-card">
                                     <img
-                                        src="/"
+                                        src={container}
                                         alt="Container"
                                         className="product-image"
                                     />
@@ -176,24 +177,28 @@ function App() {
             </div>
 
 
-            <footer className="footer">
-                <div className="footer-content">
+            <footer className="footer bg-[#595F65] text-white">
+                <div className="footer-content flex justify-between">
                     <div className="footer-brand">
                         <h2>Cargo Containers</h2>
                         <p>Купівля та оренда контейнерів онлайн</p>
                     </div>
-                    <nav className="footer-nav">
-                        <a href="#">Про нас</a>
-                        <a href="#">Контакти</a>
-                        <a href="#">Оголошення</a>
-                    </nav>
-                    <div className="footer-social">
-                        <a href="#"><img src="google-icon.svg" alt="Google"/></a>
-                        <a href="#"><img src="facebook-icon.svg" alt="Facebook"/></a>
-                        <a href="#"><img src="twitter-icon.svg" alt="Twitter"/></a>
-                        <a href="mailto:contact@cargocontainers.com">contact@cargocontainers.com</a>
+                    <div className="flex flex-col items-center">
+                        <nav className="footer-nav flex flex-row gap-10">
+                            <a href="#">Про нас</a>
+                            <a href="#">Контакти</a>
+                            <a href="#">Оголошення</a>
+                        </nav>
+                        <div className="footer-social flex flex-row gap-5">
+                            <BrandGithub />
+                            <BrandGoogle />
+                            <BrandFacebook />
+                            <BrandTwitter />
+                            <a href="mailto:contact@cargocontainers.com">contact@cargocontainers.com</a>
+                        </div>
                     </div>
                 </div>
+                <br className="text-white" />
                 <div className="footer-bottom">
                     <p>© 2023 Cargo Containers. Усі права захищені.</p>
                 </div>
