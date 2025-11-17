4ab3f8a1f4a094ab51b46c6075545957330b4427
VikaBilyk <128689260+VikaBilyk@users.noreply.github.com>
2025-11-17 19:18:16 +0200

    fix: schema notification

--- server/schemas/notification.py
+++ server/schemas/notification.py
@@ -1,6 +1,6 @@
 import datetime
 from enum import Enum
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, ConfigDict
 from typing import Dict, Any
 
 from server.models.enums.notification_type import NotificationType
@@ -17,9 +17,8 @@ class UserNotificationCreate(UserNotificationBase):
 
 
 class UserNotificationGet(UserNotificationBase):
+    model_config = ConfigDict(from_attributes=True)
+
     id: int
     created_at: datetime.datetime
     read_at: datetime.datetime | None = None
-
-    class Config:
-        from_attributes = True
