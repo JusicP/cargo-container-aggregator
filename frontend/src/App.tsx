b2ac76a5aa680327dbdf29637c4870841c37f3a6
OnishchenkoDaria <daria.onishchenko.57@gmail.com>
2025-12-03 02:49:53 +0200

    feat: adding top layer toast message

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,15 +4,17 @@ import AppRouter from '@/router/AppRouter';
 import Navbar from "@/router/Navbar.tsx"
 import Footer from "@/router/Footer.tsx"
 import { Box, Flex } from "@chakra-ui/react";
+import { Toaster } from "@/components/ui/toaster";
 
 const App: React.FC = () => {
   return (
     <Flex direction="column" minH="100vh">
         <Navbar />
           <Box flex="1">
-              <AppRouter /> 
+              <AppRouter />
           </Box>
         <Footer />
+        <Toaster />
     </Flex>
   );
 };
